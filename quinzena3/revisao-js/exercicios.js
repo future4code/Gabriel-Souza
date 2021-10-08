8ac434e28484a7a6de3ac7bcb863109aa7fca3be
Gabriel Silva <gabriel127.gk@gmail.com>
2021-10-07 21:54:21 -0300

    Atualizando-Lista-De-Exercicios-Js

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -96,67 +96,61 @@ const e = !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
 // EXERCÍCIO 07
 function retornaNNumerosPares(n) {
 
+  // Incompleto
+
 
 }
 
 // EXERCÍCIO 08
 function checaTriangulo(a, b, c) {
 
- const triangulo = (a1, a2, a3) => {
-   if (a1 / a1  === a1){
-     return "Equilátero"
-   } else if (a2 / a2  === a2){
-     return "Isósceles"
-   } else if (a3 / a3 !== a3) {
-    return "Escaleno"
-   }
+  const funcaoDescobreTriangulo = (a1, b2, c3) => {
 
-   }
-   return triangulo(a, b, c)
+    if (a1 === b2 && a1 === c3){
+      return "Equilátero"
+    } else if (a1 === b2){
+      return "Isósceles"
+    } else {
+      return "Escaleno"
+    }
+  };
+  
+  return funcaoDescobreTriangulo(a, b, c);
+   
   // return 'Escaleno'
   // return 'Equilátero'
   // return 'Isósceles'
-}
+};
 
 // EXERCÍCIO 09
 function comparaDoisNumeros(num1, num2) {
 
-const maiorNumero = (n1, n2) => {
 
-  if (n1 > n2) {
-    return n1
-  } else {
-    return n2
-  }
+ let maiorN 
+ let menorN 
 
-}
-const maior = maiorNumero(num1, num2);
-/////////////////////////////////////////////////////////////////////////////////////////
-const maiorDivisivelPorMenor = (n3, n4) => {
+ if (num1 > num2) {
+   maiorN = num1
+   menorN = num2
 
-  if (n3 > n4){
-    return n3 % n4 === 0
-  } else if (n4 > n3){
-    return n4 % n3 === 0
-  }
+ } else {
+   menorN = num1
+   maiorN = num2
+ }
 
-}
-const maiorDivisivelPorMenor1 = maiorDivisivelPorMenor(num1, num2)
-////////////////////////////////////////////////////////////////////////////////////////////////
-const diferencaNumeros = (n5, n6) => {
+ let eDivisivel = maiorN % menorN === 0;
 
-  if (n5 >= n6 || n5 <= n6){
-    return  n5, n6
-  }
+ let eDiferente = maiorN - menorN
 
-}
-const diferencaNumeros1 = diferencaNumeros(num1, num2)
-///////////////////////////////////////////////////////////////////////////////////////////////
-const objeto = {maiorNumero: maior,
-  maiorDivisivelPorMenor: maiorDivisivelPorMenor1,
-  diferenca: diferencaNumeros1}
+const objeto = {
+  maiorNumero: maiorN,
+  maiorDivisivelPorMenor: eDivisivel,
+   diferenca: eDiferente
+  
+};
 
-  return objeto
+return objeto;
+
 
   // Formato do objeto a ser retornado:
   // {
@@ -168,20 +162,22 @@ const objeto = {maiorNumero: maior,
 
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
+
+  // Imcompleto
   
-  const arrayMaior = (array1) => {
-    return Math.max.apply(Math, array1);
-    };
-const arrayMaior1 = arrayMaior(array);
+ let newArray = [];
 
-    const arrayMenor = (array2) => {
-      return Math.min.apply(Math, array2);
-      };
-const arrayMenor1 = arrayMenor(array);
+for (let i = 0; i < array.length; i++) {
 
-const newArray = [arrayMaior1, arrayMenor1]
+  let a = array[array.length - 2];
+  let b = array[array.length - 6];
 
-return newArray
+  newArray.push(a)
+  newArray.push(b)
+
+  break;
+}
+return newArray;
 
 }
 
@@ -189,74 +185,240 @@ return newArray
 // EXERCÍCIO 11
 function ordenaArray(array) {
 
-}
+  let arrayOrdenado = array.sort((a, b) =>{
+   return a - b
+  });
+  return arrayOrdenado
+
+};
 
 // EXERCÍCIO 12
 function filmeFavorito() {
 
-}
+  const objetoFilme = {
+
+    nome:"O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
+
+  };
+
+  return objetoFilme;
+
+};
 
 // EXERCÍCIO 13
 function imprimeChamada() {
-  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
-}
+  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELEN
+  
+  const frasePegandoObjetoFilme = filmeFavorito()
+
+    const funcaoImprimeFrase = (filme) =>{
+
+    const frase = `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
+    return frase;
+
+    };
+
+  const filmefrase = funcaoImprimeFrase(frasePegandoObjetoFilme);
+  return filmefrase
+
+};
 
 // EXERCÍCIO 14
 function criaRetangulo(lado1, lado2) {
 
-}
+  const objetoRetangulo = {
+
+    largura: lado1,
+    altura: lado2,
+    perimetro: 2 * (lado1 + lado2),
+    area: (lado1 * lado2)
+
+  };
+
+  return objetoRetangulo;
+
+};
 
 // EXERCÍCIO 15
 function anonimizaPessoa(pessoa) {
 
-}
+  const objetoPessoaAnonima = {
+   ...pessoa,
+   nome: "ANÔNIMO"
+  };
+
+  return objetoPessoaAnonima;
+
+};
 
 // EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {
 
-}
+    const maioressDe18 = arrayDePessoas.filter((idade) =>{
+      return idade.idade >= 18
+     });
+  
+ return maioressDe18;
+ 
+};
 
 // EXERCÍCIO 16B
 function menoresDe18(arrayDePessoas) {
+
+  const menorDe18 =  arrayDePessoas.filter((idade2) =>{
+    return idade2.idade < 18
+  });
+
+  return menorDe18;
 
 }
 
 // EXERCÍCIO 17A
 function multiplicaArrayPor2(array) {
 
-}
+  let a = [];
+
+  for(let i = 0; i < array.length; i++) {
+  a.push(array[i] * 2);
+  };
+
+  return a
+
+};
 
 // EXERCÍCIO 17B
 function multiplicaArrayPor2S(array) {
 
-}
+  let a = [];
+
+  for(let i = 0; i < array.length; i++) {
+    
+  a.push(array[i] * 2)
+  
+  };
+
+const aString = a.map(array1)
+
+function array1(elemento) {
+
+  return elemento.toString()
+};
+
+return aString
+
+};
 
 // EXERCÍCIO 17C
 function verificaParidade(array) {
 
-}
+ let a = [...array];
+
+ let aFrase = a.map(array5)
+ function array5(element) {
+
+  let y = element + 0;
+
+  if (y % 2 === 0) {
+    return y + " é par"
+
+  }else if (y % 2 !== 0) {
+    return y + " é ímpar"
+
+    };
+
+ };
+ return aFrase
+
+};
 
 // EXERCÍCIO 18A
 function retornaPessoasAutorizadas(pessoas) {
 
-}
+  const pessoasComPermissoes = pessoas.filter((pessoasP) =>{
+
+   let a = pessoasP.altura >= 1.5
+   let b = pessoasP.idade > 14
+   let c = pessoasP.idade < 60
+
+   return a && b && c
+
+  });
+
+  return pessoasComPermissoes;
+
+};
 
 // EXERCÍCIO 18B
 function retornaPessoasNaoAutorizadas(pessoas) {
+  
+const pessoasNaoAutorizadas = pessoas.filter((pessoasN) =>{
 
-}
+   let a = pessoasN.altura < 1.5
+   let b = pessoasN.idade <= 14
+   let c = pessoasN.idade > 60
+
+   return a || b || c
+
+  });
+
+  return pessoasNaoAutorizadas;
+
+};
 
 // EXERCÍCIO 19A
 function ordenaPorNome(consultasNome) {
 
-}
+const arrayOrdenado  = consultasNome.sort((a, b) => {
+
+  return (a.nome > b.nome) ? 1 : ((b.nome > a.nome) ? -1 : 0 );
+});
+
+return arrayOrdenado;
+
+};
 
 // EXERCÍCIO 19B
 function ordenaPorData(consultasData) {
+
+
+
+let ac = [...consultasData]
+
+// Incompleto
+
+ ac.sort(function(a,b) { 
+    return a.dataDaConsulta.getTime() - b.dataDaConsulta.getTime() 
+
+  })
+
+  return ac
 
 }
 
 // EXERCÍCIO 20
 function calculaSaldo(contas) {
+
+  // Icompleto
+
+let a = contas.map((valores)=> {
+  return valores.compras
+})
+
+let b = contas.map((subT)=> {
+
+  return subT.saldoTotal
+})
+
+a.forEach(funcao)
+function funcao(item, index, array) {
+
+  return array[index]  = item / a
+}
+
+console.log(a)
+return a
 
 }
