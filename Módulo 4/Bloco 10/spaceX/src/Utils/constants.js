129f4331117e5f5c083e8a038fe0357a03608788
Gabriel Silva <gabriel127.gk@gmail.com>
2022-02-01 20:24:01 -0300

    Foi errado desculpe

--- Módulo 4/Bloco 10/spaceX/src/Utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const GetTrips = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/trips";
-export const GetTripDetail = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/trip";
-export const Populate = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/:aluno/populate";
-export const CreateTrip = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/trips";
-export const ApplyToTrip = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/trips";
-export const DeleteTrip = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/trips";
-export const LoginUser = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/login";
-export const Decide = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/trips";
