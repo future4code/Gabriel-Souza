1502abb51dc3d5b9521ebf5ee4fbf146f0e892a7
Gabriel Silva <gabriel127.gk@gmail.com>
2022-04-28 22:38:11 -0300

    1° Commit. Projeto: LabECommerce. Configurações Feitas.

---
+++ Módulo 6/Bloco 17/LabECommerce/src/fuctions.ts
@@ -0,0 +1,23 @@
+export const removeSpace = ( value: string ): boolean => {
+    return /\s/g.test(value)
+};
+
+export const convertDate = ( date: string ): any => {
+    return date.split("/").reverse().join("-");
+};
+
+const date: Date = new Date();
+const  day = String(date.getDate()).padStart(2, '0');
+const month = String(date.getMonth() + 1).padStart(2, '0');
+const year = date.getFullYear();
+
+export const currentDate = year + "-" + month + "-" + day;
+
+export const revertDate = ( date: any ): any => {
+    const taskDate: any = new Date(date);
+    const  day = String(taskDate.getDate()).padStart(2, '0');
+    const month = String(taskDate.getMonth() + 1).padStart(2, '0');
+    const year = taskDate.getFullYear();
+    const currentDate = day + "/" + month + "/" + year;
+    return currentDate;
+}; 
